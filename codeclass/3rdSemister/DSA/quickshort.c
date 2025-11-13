ab38591199cbff35f373b988daf71b0e6316db5a
Akash Maity <maityakashkumar2005@gmail.com>
2025-11-13 20:26:15 +0530

    Refactor quicksort implementation and input handling

--- codeclass/3rdSemister/DSA/quickshort.c
+++ codeclass/3rdSemister/DSA/quickshort.c
@@ -1,44 +1,63 @@
 #include<stdio.h>
-void swap(int *a,int *b){
-    int temp=*a;
-    *a=*b;
-    *b=temp;
+
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
-void quicksort(int a[],int beg,int end){
-    if(beg<end){
-        int p=partition(a,beg,end);
-        quicksort(a,beg,p);
-        quicksort(a,p+1,end);
-    }
-}
-int partition(int a[],int beg,int end){
-    int down=beg-1,up=end+1;
-    int pivot=a[beg];
+
+int partition(int a[], int beg, int end){
+    int down = beg - 1, up = end + 1;
+    int pivot = a[beg];
+
     while(1){
         do{
             --up;
-        }while(a[up]>pivot && up>=beg);
+        } while(a[up] > pivot && up >= beg);
+
         do{
             ++down;
-        }while(a[down]<pivot && down<=end);
-        if(down<up){
-            swap(&a[down],&a[up]);
+        } while(a[down] < pivot && down <= end);
+
+        if(down < up){
+            swap(&a[down], &a[up]);
         }
         else{
             return up;
         }
     }
 }
-void display(int a[],int n){
-    for(int i=0;i<n;i++){
-        printf("%d ",a[i]);
+
+void quicksort(int a[], int beg, int end){
+    if(beg < end){
+        int p = partition(a, beg, end);
+        quicksort(a, beg, p);
+        quicksort(a, p + 1, end);
+    }
+}
+
+void display(int a[], int n){
+    for(int i = 0; i < n; i++){
+        printf("%d ", a[i]);
     }
     printf("\n");
 }
-int main()
-{
-    int a[5]={5,4,3,2,1};
-    quicksort(a,0,4);
-    display(a,5);
+
+int main(){
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    quicksort(a, 0, n - 1);
+
+    printf("Sorted array: ");
+    display(a, n);
+
     return 0;
 }
